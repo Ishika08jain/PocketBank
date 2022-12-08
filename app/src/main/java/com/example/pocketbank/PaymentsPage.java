c1adba7decd3028a2a64cd16300f9caaea7d0003
muskanlalwani <muskan.lalwani2020@vitbhopal.ac.in>
2022-12-08 21:24:06 +0530

    pocketbank

--- app/src/main/java/com/example/pocketbank/PaymentsPage.java
+++ app/src/main/java/com/example/pocketbank/PaymentsPage.java
@@ -1,14 +1,35 @@
 package com.example.pocketbank;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class PaymentsPage extends AppCompatActivity {
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimerTask;
+
+public class PaymentsPage extends AppCompatActivity {
+    ImageSlider imageSlider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payments_page);
+
+
+        imageSlider =findViewById(R.id.image_slider);
+
+        ArrayList<SlideModel> imageList = new ArrayList<>();
+
+
+        imageList.add(new SlideModel(R.drawable.image1,null));
+        imageList.add(new SlideModel(R.drawable.image3,null));
+        imageList.add(new SlideModel(R.drawable.pocket,null));
+
+        imageSlider.setImageList(imageList);
     }
 }
